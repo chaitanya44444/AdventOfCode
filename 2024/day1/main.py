3a94bbe9d3ac31f2280e5a1e2cfdb61db33635af
Chaitanya Vaswani <vaswanichaitanya@gmail.com>
2024-12-01 13:30:26 +0530

    made it smaller

--- 2024/day1/main.py
+++ 2024/day1/main.py
@@ -11,12 +11,8 @@ l2.sort(reverse = True)
 a=0
 b=0
 #part 1
+print("answer for part 1:",str(sum(abs(l1[i] - l2[i]) for i in range(len(l1))) ))
 
-for i in range(len(l1)):
-    a+=abs(l1[i]-l2[i])
-print(f"part 1 answer is {a}")
 
 #part 2
-for i in range(len(l1)):
-    b+=l2.count(l1[i]) * l1[i]
-print(f"answer for part 2: {b}")
+print("Answer for part 2:", sum(l2.count(l1[i]) * l1[i] for i in range(len(l1))))
